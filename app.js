4c9a00ee6fd3c8e8d82a1b297e8b245c73a45368
Klim <tsttony85@gmail.com>
2021-10-02 21:31:22 +0300

    010 - auth and security finished

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 
@@ -9,17 +13,52 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-// 1) MIDDLEWARE
-console.log(`>>CURRENT ENV ${process.env.NODE_ENV} <<<`);
+// 1) GLOBAL MIDDLEWARES
+// Security HTTP headers
+
+app.use(helmet());
+
+// Development logging
+console.log(`CURRENT ENV >> ${process.env.NODE_ENV} <<`);
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour'
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+  whitelist: [
+    'duration',
+    'ratingsQuantity',
+    'ratingsAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price'
+    ]
+  })
+);
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.headers);
+
   next();
 });
 
