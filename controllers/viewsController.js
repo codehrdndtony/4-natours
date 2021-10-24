1ee2af07ac124ec4fd55d389ff0654dc37868f6f
Klim <tsttony85@gmail.com>
2021-10-24 20:36:01 +0300

    Added tour page functionality - content, map

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,11 +14,22 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get data for the requested tour (incl reviews and guides)
+  const tour = await Tour.findOne({slug: req.params.slug}).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  });
+
   // 2) Build template
   // 3) Render the template using data from UI
-    res.status(200).render('tour', {
-      title: 'The Forest Hiker Tour'
+    res.status(200)
+       .set(
+         'Content-Security-Policy',
+         "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+       )
+       .render('tour', {
+      title: `${tour.name} Tour`,
+      tour
     });
-};
+});
---
+++ public/js/mapbox.js
@@ -0,0 +1,49 @@
+/* esLint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidHN0dG9ueTg1IiwiYSI6ImNrdjVmcnc4bTJuMDUybnF3MGkwY2RycmgifQ.sdSSEMBALgXYVF55mJOq3g';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/tsttony85/ckv5h9d553xj515qqoj1u5iol',
+  scrollZoom: false
+  // center: [-118.113491,34.111745],
+  // zoom: 4,
+  // interactive: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map)
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, { padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100
+  }
+});
